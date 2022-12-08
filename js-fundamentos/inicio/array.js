6b55de1eb69acf0c265957ccdc4e410902fa88f2
JoãoVictorKarline <jvkarline23@gmail.com>
2022-12-08 04:16:28 -0300

    Vetores

--- js-fundamentos/inicio/array.js
+++ js-fundamentos/inicio/array.js
@@ -4,6 +4,6 @@ console.log(vetor[1])
 
 vetor[4] = 3 // Vetor no índice 4 vai receber o dado 3
 
-vetor.push(3)
+vetor.push(123) // Vetor adiciona no Próximo índice o dado entre Parêntes
 
-console.log(vetor)
+console.log(vetor.pop()) // Mostra o último elemento
