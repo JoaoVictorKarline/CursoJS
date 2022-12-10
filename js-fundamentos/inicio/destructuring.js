5f0581e4c52cb9444e9fc358863797966d299d18
JoãoVictorKarline <jvkarline23@gmail.com>
2022-12-10 14:21:33 -0300

    relacionais

---
+++ js-fundamentos/inicio/destructuring.js
@@ -0,0 +1,9 @@
+const pessoa = {
+    nome: 'João',
+    email: 'josjojsdo@',
+    idade: 12
+}
+
+const {nome, idade } = pessoa // Tire do objeto o atributo Nome e idade e criei uma variável pra ele
+
+console.log(nome, idade)
