4f809a8cc972087ca4c4005f07b55554d61db562
JoãoVictorKarline <jvkarline23@gmail.com>
2022-12-17 03:28:43 -0300

    while

---
+++ js-fundamentos/inicio/exercicioswhile/while01.js
@@ -0,0 +1,8 @@
+let nome = 'João'
+let senha = 'João'
+
+while(senha==nome){
+    console.log('Senha igual o nome, tente novamente')
+    senha = ''
+}
+
---
+++ js-fundamentos/inicio/exercicioswhile/while03.js
@@ -0,0 +1,6 @@
+let limite = 50
+let soma = 0
+while(soma<limite){
+    soma++
+    console.log(soma)
+}
