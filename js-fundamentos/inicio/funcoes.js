dab0478082ba760bd28e31b41e1a2dad50db8b95
JoãoVictorKarline <jvkarline23@gmail.com>
2022-12-26 22:35:19 -0300

    funcoesinicio

---
+++ js-fundamentos/inicio/funcoes.js
@@ -0,0 +1,15 @@
+// Bloco de código que executa uma determinada tarefa 
+// Cada bloco executa uma função diferente
+// Ex: Função de somar, função de multiplicar...
+
+function parimpar(n){
+    if(n%2==0){
+        return 'Par'
+    }else{
+        return 'Ímpar'
+    }
+}
+
+let a = parimpar(10)
+console.log(a)
+
