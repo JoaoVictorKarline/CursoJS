67fa9470104c9fa41c741b7c5b5c2da06500052d
JoãoVictorKarline <jvkarline23@gmail.com>
2023-01-24 00:38:59 -0300

    domcopiar

---
+++ js-fundamentos/DOM/projetocopiadom/script.js
@@ -0,0 +1,28 @@
+let but = document.querySelector('#botao')
+let divconteudo = [...document.querySelectorAll('.bt')]
+console.log(divconteudo)
+let divright = document.querySelector('.containerright')
+let divleft = document.querySelector('.containerleft')
+
+divconteudo.map((ele)=>{
+    ele.addEventListener("click", function elclick(evt){
+        
+        let el = evt.target
+        el.classList.toggle('format') // Se o elemento estiver com a classe format vai ser removido a classe, se não vai ser adicionado
+    }) 
+    
+})
+
+
+
+but.addEventListener("click", ()=>{
+    let selecionados = [...document.querySelectorAll('.format')]
+
+    selecionados.map((ele)=>{
+        divright.appendChild(ele)
+        divright.style.flexDirection = 'column'
+    })
+
+    
+
+})
